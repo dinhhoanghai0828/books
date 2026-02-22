dbf3c03db29dfb10147cf9cffc4c5d54197a99c9
haidh8 <dinhhoanghai0828@gmail.com>
2026-02-22 23:24:56 +0700

    22/02/2026

--- start_backend.bat
+++ start_backend.bat
@@ -1,9 +1,9 @@
 echo START BACKEND JAVA
-cd /d G:\20_PROJECT\books\1_BACK_END
+cd /d D:\20_PROJECT\books\1_BACK_END
 echo BUILD PROJECT
 call mvn clean install -DskipTests
 
 echo RUN WAR/JAR
-java -jar G:\20_PROJECT\books\1_BACK_END\target\demo.war
+java -jar D:\20_PROJECT\books\1_BACK_END\target\demo.war
 
 pause
