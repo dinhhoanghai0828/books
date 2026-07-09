d9ba7ce2c821c85defd97a47456368385396ed24
haidh8 <dinhhoanghai0828@gmail.com>
2026-07-09 23:31:20 +0700

    09/07/2026

--- 1_BACK_END/deploy.bat
+++ 1_BACK_END/deploy.bat
@@ -1,17 +1,23 @@
-@rem C:\Windows\System32\cmd.exe\PLINK.EXE 91.108.111.253
-@rem I:\books\2_books\1_BACK_END\putty\PLINK.EXE 91.108.111.253
-@rem mvn clean install -DskipTests
-set userName=root
-set password=Thanphong@93
-set server_ip=91.108.111.253
-
-call %~dp0\putty\plink.exe -batch -ssh %userName%@%server_ip% -pw %password% "sudo chmod 777 /var/www/backend"
-call %~dp0\putty\plink.exe -batch -ssh %userName%@%server_ip% -pw %password% "sudo rm -rf /var/www/backend/demo.war"
-call %~dp0\putty\pscp.exe -pw %password% %~dp0\target\demo.war %userName%@%server_ip%:/var/www/backend
-call %~dp0\putty\plink.exe -batch -ssh %userName%@%server_ip% -pw %password% "sudo kill -9 `sudo lsof -t -i:8080`"
-call %~dp0\putty\plink.exe -batch -ssh %userName%@%server_ip% -pw %password% "nohup java -jar /var/www/backend/demo.war  &"
-call %~dp0\putty\plink.exe -batch -ssh %userName%@%server_ip% -pw %password% "sudo tail -500f /var/www/backend/nohup.out"
+@echo off
 
 
+set userName=haidh8
+set password=haiha92
+set server_ip=192.168.1.14
 
+REM Xoa file cu
+call "%~dp0putty\plink.exe" -batch -ssh %userName%@%server_ip% -pw %password% "rm -f /var/www/backend/demo.war"
 
+REM Copy war len server
+call "%~dp0putty\pscp.exe" -batch -pw %password% "%~dp0target\demo.war" %userName%@%server_ip%:/var/www/backend/
+
+REM Kill app cu
+call "%~dp0putty\plink.exe" -batch -ssh %userName%@%server_ip% -pw %password% "pkill -f demo.war || true"
+
+REM Chay app
+call "%~dp0putty\plink.exe" -batch -ssh %userName%@%server_ip% -pw %password% "nohup java -jar /var/www/backend/demo.war >/var/www/backend/nohup.out 2>&1 &"
+
+REM Xem log
+call "%~dp0putty\plink.exe" -batch -ssh %userName%@%server_ip% -pw %password% "tail -f /var/www/backend/nohup.out"
+
+pause
