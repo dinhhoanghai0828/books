00a2ca50afd041c476bda8b3b97ace4a3726fd67
haidh8 <dinhhoanghai0828@gmail.com>
2026-07-20 21:10:23 +0700

    20/07/2026

--- service tu chay/deploy.bat
+++ service tu chay/deploy.bat
@@ -1,5 +1,5 @@
 @echo off
-title Start Nginx + Cloudflared
+title Start Nginx + Frontend + Backend + Cloudflared
 
 echo ===============================
 echo Stopping Nginx...
@@ -16,6 +16,20 @@ echo ===============================
 start "" nginx.exe
 
 timeout /t 2 >nul
+
+echo.
+echo ===============================
+echo Starting Frontend...
+echo ===============================
+
+start "Frontend" cmd /k "cd /d D:\20_PROJECT\books\2_FRONT_END && npm run dev"
+
+echo.
+echo ===============================
+echo Starting Backend...
+echo ===============================
+
+start "Backend" cmd /k "cd /d D:\20_PROJECT\books\1_BACK_END && echo BUILD PROJECT && call mvn clean install -DskipTests && echo. && echo RUN WAR/JAR && java -jar target\demo.war"
 
 echo.
 echo ===============================
