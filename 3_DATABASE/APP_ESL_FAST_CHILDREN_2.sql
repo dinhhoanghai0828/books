e03472d84f88c75819176dec1b8b479d0c5de930
haidh8 <dinhhoanghai0828@gmail.com>
2026-07-26 15:08:38 +0700

    26/07/2026

--- 3_DATABASE/APP_ESL_FAST_CHILDREN_2.sql
+++ 3_DATABASE/APP_ESL_FAST_CHILDREN_2.sql
@@ -65,4 +65,78 @@ INSERT INTO CONTENTS (ENG,VI,START_TIME,END_TIME,VOLUME_SLUG) VALUES
 	('He puts a snack in his backpack','Cậu bé bỏ một món ăn nhẹ vào ba lô','00:00:21.510','00:00:24.830','esl-fast-beginner-2-6'),
 	('He might get hungry','Có thể cậu bé sẽ thấy đói','00:00:25.590','00:00:27.830','esl-fast-beginner-2-6'),
 	('He puts a notebook in his backpack','Cậu bé bỏ một quyển vở vào ba lô','00:00:27.530','00:00:32.490','esl-fast-beginner-2-6'),
-	('He is ready to study','Cậu bé đã sẵn sàng để học','00:00:33.190','00:00:36.210','esl-fast-beginner-2-6');
+	('He is ready to study','Cậu bé đã sẵn sàng để học','00:00:33.190','00:00:36.210','esl-fast-beginner-2-6'),
+
+	/* (UUID(),'esl-fast-beginner-2-7','Studying at the Library','Học ở thư viện','BEGINNER_02_ESL_FAST_CHILDREN_007.mp3',NULL,'00:00:00.000','00:00:00.000','esl-fast-children-2','NO',7) */
+	('He is at the library','Cậu ấy đang ở thư viện','00:00:00.520','00:00:02.660','esl-fast-beginner-2-7'),
+	('There are a lot of people','Ở đó có rất nhiều người','00:00:03.100','00:00:05.940','esl-fast-beginner-2-7'),
+	('Everyone is studying for their final exams','Mọi người đều đang ôn thi cuối kỳ','00:00:07.220','00:00:10.940','esl-fast-beginner-2-7'),
+	('He finally finds a seat','Cuối cùng cậu ấy cũng tìm được một chỗ ngồi','00:00:11.700','00:00:14.800','esl-fast-beginner-2-7'),
+	('He sits down','Cậu ấy ngồi xuống','00:00:15.860','00:00:17.580','esl-fast-beginner-2-7'),
+	('He takes out his book','Cậu ấy lấy sách ra','00:00:18.500','00:00:20.640','esl-fast-beginner-2-7'),
+	('He takes out a pen','Cậu ấy lấy một cây bút ra','00:00:21.460','00:00:24.100','esl-fast-beginner-2-7'),
+	('He takes out a highlighter','Cậu ấy lấy một cây bút dạ quang ra','00:00:24.780','00:00:27.940','esl-fast-beginner-2-7'),
+	('He highlights the important parts','Cậu ấy tô nổi bật những phần quan trọng','00:00:28.680','00:00:32.500','esl-fast-beginner-2-7'),
+	('Then, he takes a study break','Sau đó, cậu ấy nghỉ giải lao một lúc','00:00:33.040','00:00:36.260','esl-fast-beginner-2-7'),
+
+	/* (UUID(),'esl-fast-beginner-2-8','Taking Notes','Ghi chú','BEGINNER_02_ESL_FAST_CHILDREN_008.mp3',NULL,'00:00:00.000','00:00:00.000','esl-fast-children-2','NO',8) */
+	('He takes out his notebook','Cậu ấy lấy quyển vở ra','00:00:00.010','00:00:02.790','esl-fast-beginner-2-8'),
+	('He reads his textbook','Cậu ấy đọc sách giáo khoa','00:00:02.890','00:00:05.610','esl-fast-beginner-2-8'),
+	('He copies the chapter title on top of the page','Cậu ấy chép tiêu đề chương lên đầu trang','00:00:06.570','00:00:11.390','esl-fast-beginner-2-8'),
+	('He writes down the important information','Cậu ấy ghi lại những thông tin quan trọng','00:00:12.010','00:00:15.850','esl-fast-beginner-2-8'),
+	('He highlights words he should remember','Cậu ấy tô nổi bật những từ cần ghi nhớ','00:00:16.570','00:00:20.130','esl-fast-beginner-2-8'),
+	('He turns the page','Cậu ấy lật sang trang tiếp theo','00:00:20.550','00:00:23.390','esl-fast-beginner-2-8'),
+	('He takes more notes','Cậu ấy ghi chép thêm','00:00:24.350','00:00:26.930','esl-fast-beginner-2-8'),
+	('He writes a summary on the chapter at the end','Cuối cùng, cậu ấy viết phần tóm tắt của chương','00:00:26.630','00:00:32.450','esl-fast-beginner-2-8'),
+
+	/* (UUID(),'esl-fast-beginner-2-9','She Writes an Essay','Cô ấy viết một bài luận','BEGINNER_02_ESL_FAST_CHILDREN_009.mp3',NULL,'00:00:00.000','00:00:00.000','esl-fast-children-2','NO',9) */
+	('She is taking an English class','Cô bé đang học một lớp tiếng Anh','00:00:00.070','00:00:03.470','esl-fast-beginner-2-9'),
+	('She has to write an essay','Cô bé phải viết một bài luận','00:00:03.710','00:00:06.330','esl-fast-beginner-2-9'),
+	('The essay is on penguins','Bài luận nói về loài chim cánh cụt','00:00:07.490','00:00:09.870','esl-fast-beginner-2-9'),
+	('She finds information on penguins from the Internet','Cô bé tìm thông tin về chim cánh cụt trên Internet','00:00:10.550','00:00:15.370','esl-fast-beginner-2-9'),
+	('She finds information on penguins from books','Cô bé tìm thông tin về chim cánh cụt trong sách','00:00:16.270','00:00:21.090','esl-fast-beginner-2-9'),
+	('Her first sentence is a riddle about penguins','Câu đầu tiên là một câu đố về chim cánh cụt','00:00:21.790','00:00:26.390','esl-fast-beginner-2-9'),
+	('Her last sentence is the answer to the riddle','Câu cuối cùng là lời giải cho câu đố đó','00:00:26.090','00:00:31.950','esl-fast-beginner-2-9'),
+
+	/* (UUID(),'esl-fast-beginner-2-10','He Takes a Test','Anh ấy làm bài kiểm tra','BEGINNER_02_ESL_FAST_CHILDREN_010.mp3',NULL,'00:00:00.000','00:00:00.000','esl-fast-children-2','NO',10) */
+	('It is Lily\'s birthday','Hôm nay là sinh nhật của Lily','00:00:00.390','00:00:02.850','esl-fast-beginner-2-10'),
+	('She gets out of bed','Cô bé bước xuống giường','00:00:03.510','00:00:05.310','esl-fast-beginner-2-10'),
+	('Nobody says anything to her','Không ai nói gì với cô bé','00:00:05.990','00:00:08.090','esl-fast-beginner-2-10'),
+	('Her mom takes her to school','Mẹ đưa cô bé đến trường','00:00:08.710','00:00:10.650','esl-fast-beginner-2-10'),
+	('Her friends do not say anything','Các bạn của cô bé cũng không nói gì','00:00:11.250','00:00:13.510','esl-fast-beginner-2-10'),
+	('The teacher does not say anything','Cô giáo cũng không nói gì','00:00:13.950','00:00:16.590','esl-fast-beginner-2-10'),
+	('Lily is sad','Lily cảm thấy buồn','00:00:17.430','00:00:18.790','esl-fast-beginner-2-10'),
+	('Lily walks home alone','Lily một mình đi bộ về nhà','00:00:19.570','00:00:21.270','esl-fast-beginner-2-10'),
+	('She opens the door','Cô bé mở cửa','00:00:21.970','00:00:23.610','esl-fast-beginner-2-10'),
+	('Her friends and family are holding a cake','Bạn bè và gia đình cô bé đang cầm một chiếc bánh kem','00:00:24.330','00:00:27.250','esl-fast-beginner-2-10'),
+	('They say, surprise Lily','Mọi người đồng thanh nói: "Bất ngờ chưa, Lily!"','00:00:26.950','00:00:30.190','esl-fast-beginner-2-10');
+
+/*=====================================================================================================================================================================================================================================================*/
+
+INSERT INTO CONTENTS (ENG,VI,START_TIME,END_TIME,VOLUME_SLUG) VALUES
+	/* (UUID(),'esl-fast-beginner-2-11','Punching a Wall','Đấm vào tường','BEGINNER_02_ESL_FAST_CHILDREN_011.mp3',NULL,'00:00:00.000','00:00:00.000','esl-fast-children-2','NO',11) */
+	('Kate is walking','Kate đang đi dạo','00:00:00.650','00:00:02.590','esl-fast-beginner-2-11'),
+	('She sees a dog','Cô bé nhìn thấy một chú chó','00:00:03.210','00:00:04.930','esl-fast-beginner-2-11'),
+	('The dog wags its tail','Chú chó vẫy đuôi','00:00:05.250','00:00:07.650','esl-fast-beginner-2-11'),
+	('Kate likes the dog','Kate thích chú chó ấy','00:00:08.490','00:00:10.290','esl-fast-beginner-2-11'),
+	('It has no collar','Nó không đeo vòng cổ','00:00:10.770','00:00:12.630','esl-fast-beginner-2-11'),
+	('Kate takes it home','Kate đưa nó về nhà','00:00:13.590','00:00:15.350','esl-fast-beginner-2-11'),
+	('She washes the dog','Cô bé tắm cho chú chó','00:00:16.150','00:00:17.990','esl-fast-beginner-2-11'),
+	('She names him Toby','Cô bé đặt tên cho nó là Toby','00:00:18.670','00:00:20.510','esl-fast-beginner-2-11'),
+	('She takes Toby to the vet','Cô bé đưa Toby đến bác sĩ thú y','00:00:21.270','00:00:24.050','esl-fast-beginner-2-11'),
+	('Toby is healthy','Toby rất khỏe mạnh','00:00:24.870','00:00:26.810','esl-fast-beginner-2-11'),
+	('Kate walks Toby every day','Ngày nào Kate cũng dắt Toby đi dạo','00:00:27.350','00:00:30.670','esl-fast-beginner-2-11'),
+	('They love each other','Cả hai rất yêu quý nhau','00:00:31.390','00:00:32.730','esl-fast-beginner-2-11'),
+
+	/* (UUID(),'esl-fast-beginner-2-12','School Supplies Box','Hộp đồ dùng học tập','BEGINNER_02_ESL_FAST_CHILDREN_012.mp3',NULL,'00:00:00.000','00:00:00.000','esl-fast-children-2','NO',12) */
+	('Ms Howard assigns a project','Cô Howard giao một bài làm theo dự án','00:00:00.260','00:00:03.580','esl-fast-beginner-2-12'),
+	('She wants the students to work in groups','Cô muốn các học sinh làm việc theo nhóm','00:00:04.380','00:00:07.339','esl-fast-beginner-2-12'),
+	('Adam works with Joe and Bill','Adam làm chung nhóm với Joe và Bill','00:00:08.160','00:00:10.700','esl-fast-beginner-2-12'),
+	('Adam is smart','Adam rất thông minh','00:00:11.320','00:00:12.840','esl-fast-beginner-2-12'),
+	('Joe draws well','Joe vẽ rất đẹp','00:00:13.560','00:00:15.160','esl-fast-beginner-2-12'),
+	('Bill is cooperative','Bill rất hợp tác','00:00:15.960','00:00:17.760','esl-fast-beginner-2-12'),
+	('They make a good group','Họ là một nhóm làm việc rất ăn ý','00:00:18.780','00:00:20.560','esl-fast-beginner-2-12'),
+	('They turn in their project','Họ nộp bài dự án','00:00:21.200','00:00:23.220','esl-fast-beginner-2-12'),
+	('Ms Howard likes it','Cô Howard rất hài lòng với bài làm','00:00:23.920','00:00:26.040','esl-fast-beginner-2-12'),
+	('She gives them a high grade','Cô cho cả nhóm điểm cao','00:00:26.460','00:00:28.920','esl-fast-beginner-2-12'),
+	('Adam, Joe, and Bill are joyful','Adam, Joe và Bill đều rất vui','00:00:29.480','00:00:33.240','esl-fast-beginner-2-12');
