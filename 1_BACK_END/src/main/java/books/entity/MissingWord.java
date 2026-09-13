1a2ed895fc6ea96886e9771c593b93ae9079169b
haidh8 <dinhhoanghai0828@gmail.com>
2026-09-13 21:31:59 +0700

    13/09/2026

---
+++ 1_BACK_END/src/main/java/books/entity/MissingWord.java
@@ -0,0 +1,22 @@
+package books.entity;
+
+public class MissingWord {
+    private Long id;
+    private String eng;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEng() {
+        return eng;
+    }
+
+    public void setEng(String eng) {
+        this.eng = eng;
+    }
+}
